30127c8b05a679235e27d3efa7cbef9dd4423fe1
nathanvdh <a1743599@student.adelaide.edu.au>
2023-11-01 20:57:56 +1030

    Add basic zone support

---
+++ custom_components/airtouch2plus/Airtouch2PlusGroupEntity.py
@@ -0,0 +1,78 @@
+from airtouch2.at2plus import At2PlusGroup
+
+from .const import DOMAIN
+
+from typing import final
+import logging
+
+from homeassistant.components.fan import FanEntity, FanEntityFeature
+from homeassistant.helpers.entity import DeviceInfo
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@final
+class AirTouch2PlusGroupEntity(FanEntity):
+    """Representation of an AirTouch 2+ zone."""
+
+    def __init__(self, group: At2PlusGroup) -> None:
+        """Initialize the fan entity."""
+        self._group = group
+
+    #
+    # Entity attributes:
+    #
+    _attr_should_poll: bool = False
+
+    #
+    # Entity overrides:
+    #
+
+    @property
+    def unique_id(self) -> str:
+        """Return unique ID for this device."""
+        return f"at2plus_group_{self._group.status.id}"
+
+    @property
+    def name(self):
+        """Return the name of this group."""
+        # TODO: Group names
+        return f"Group {self._group.status.id}"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device info for this device."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.unique_id)},
+            name=self.name,
+            manufacturer="Polyaire",
+            model="Airtouch 2+",
+        )
+
+    async def async_added_to_hass(self) -> None:
+        """Call when entity is added."""
+        # Add callback for when group receives new data.
+        # Removes callback on remove.
+        self.async_on_remove(
+            self._group.add_callback(self.async_write_ha_state))
+
+    #
+    # FanEntity overrides
+    #
+
+    @property
+    def is_on(self):
+        """Return if group is on."""
+        return self._group.is_on()
+
+    @property
+    def percentage(self) -> int:
+        """Return current percentage of the group damper."""
+        if not self.is_on():
+            return 0
+        return self._group.status.damp
+
+    @property
+    def supported_features(self) -> FanEntityFeature:
+        """Fan supported features."""
+        return 0
---
+++ custom_components/airtouch2plus/fan.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import logging
+
+from airtouch2.at2plus import At2PlusClient
+from .Airtouch2PlusGroupEntity import AirTouch2PlusGroupEntity
+from .const import DOMAIN
+
+from homeassistant.components.fan import FanEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the AirTouch 2+ group entities."""
+    client: At2PlusClient = hass.data[DOMAIN][config_entry.entry_id]
+    entities: list[FanEntity] = [
+        AirTouch2PlusGroupEntity(group) for group in client.groups_by_id.values()
+    ]
+
+    if entities:
+        async_add_entities(entities)
