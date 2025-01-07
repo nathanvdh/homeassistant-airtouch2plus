d4df0aad966575b78902d98af6c2ac0625938013
Nathan Van der Hoek <55475809+nathanvdh@users.noreply.github.com>
2025-01-07 20:49:34 +1030

    fan.py: Apply Ruffy formatter

--- custom_components/airtouch2plus/fan.py
+++ custom_components/airtouch2plus/fan.py
@@ -12,6 +12,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
